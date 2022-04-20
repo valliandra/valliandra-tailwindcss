93698072cd9fdf37637e50b6f8a694219b7aa545
ferdinadvalliandra <stevenandra9@gmail.com>
2022-04-20 20:07:08 +0700

    Upload Pertama

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ['index.html'],
+  darkMode: 'class',
+  theme: {
+    container: {
+      center: true,
+      padding: 'px',
+    },
+    extend: {
+      colors: {
+        primary: '#8b5cf6',
+        secondary: '#64748b',
+        dark: '#0f172a',
+      },
+      screens: {
+        '2xl': '1320px',
+      },
+    },
+  },
+  plugins: [],
+};
